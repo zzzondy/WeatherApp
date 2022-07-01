f66fd26664ce8ce5e63fd311dcfbd26dab40ae84
zzzondy <artemr19032006@gmail.com>
2022-07-01 12:56:26 +0300

    Добавлен красный цвет на фоне при свайпе города с погодой вправо, убраны закругленные края

--- app/src/main/java/com/weatherapp/fragments/ListOfCitiesFragment.kt
+++ app/src/main/java/com/weatherapp/fragments/ListOfCitiesFragment.kt
@@ -1,31 +1,33 @@
 package com.weatherapp.fragments
 
+import android.graphics.Canvas
+import android.graphics.Color
 import android.os.Bundle
-import android.os.Handler
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
+import androidx.core.content.ContextCompat
 import androidx.core.os.bundleOf
+import androidx.core.view.ViewCompat
 import androidx.core.view.doOnPreDraw
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.transition.MaterialContainerTransform
 import com.google.android.material.transition.MaterialElevationScale
-import com.google.android.material.transition.MaterialSharedAxis
 import com.google.gson.Gson
 import com.weatherapp.R
 import com.weatherapp.database.CityWeatherRepository
 import com.weatherapp.databinding.FragmentListOfCitiesBinding
 import com.weatherapp.fragments.adapters.CityWeatherAdapter
 import com.weatherapp.fragments.adapters.CityWeatherItemAnimator
+import com.weatherapp.fragments.adapters.CityWeatherViewHolder
 import com.weatherapp.models.entities.DatabaseCity
 import com.weatherapp.navigation.CityWeatherListener
 import com.weatherapp.providers.ResourceProvider
+import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator
 
 
 class ListOfCitiesFragment : Fragment(), CityWeatherListener {
@@ -35,11 +37,6 @@ class ListOfCitiesFragment : Fragment(), CityWeatherListener {
     private var resourceProvider: ResourceProvider? = null
     private var cityWeatherRepository: CityWeatherRepository? = null
     private var cityWeatherAdapter: CityWeatherAdapter? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -104,6 +101,34 @@ class ListOfCitiesFragment : Fragment(), CityWeatherListener {
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, swipeDir: Int) {
                 val position = viewHolder.adapterPosition
                 viewModel?.deleteCity(cityWeatherAdapter?.getCityWithId(position)!!)
+            }
+
+            override fun onChildDraw(
+                c: Canvas,
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                dX: Float,
+                dY: Float,
+                actionState: Int,
+                isCurrentlyActive: Boolean
+            ) {
+                RecyclerViewSwipeDecorator.Builder(requireContext(), c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+                    .addBackgroundColor(ContextCompat.getColor(requireContext(), R.color.deleteRed))
+                    .setIconHorizontalMargin(8)
+                    .addSwipeLeftActionIcon(R.drawable.delete)
+                    .addSwipeLeftLabel(getString(R.string.delete))
+                    .setSwipeLeftLabelColor(ContextCompat.getColor(requireContext(), R.color.white))
+                    .create()
+                    .decorate()
+                super.onChildDraw(
+                    c,
+                    recyclerView,
+                    viewHolder,
+                    dX,
+                    dY,
+                    actionState,
+                    isCurrentlyActive
+                )
             }
         }
 
